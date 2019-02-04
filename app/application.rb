1f2860ceb5a095f78da0251f656d9afb62b9dabd
Roberto Samia <bojosteph@gmail.com>
2019-02-04 02:46:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       
-       item = @@items.find{|i| i.name == item_name}
+       item = @@items.detect.name == item_name}
       
       if item.nil?
         resp.write "item not found"
